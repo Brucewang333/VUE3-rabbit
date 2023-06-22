ade80e9ac4302bbd0a56a3e648cff46ec50d9b13
Bruce <wang922711@163.com>
2023-06-23 00:30:34 +0800

    pinia优化重复请求

---
+++ vue-rabbit/src/stores/category.js
@@ -0,0 +1,18 @@
+import { defineStore } from "pinia";
+import { getCategoryAPI } from "@/apis/layout";
+import { ref } from "vue";
+export const useCategoryStore = defineStore("category", () => {
+  // 导航栏数据管理
+  // state 导航列表数据
+  const categoryList = ref([]);
+  // action 获取导航数据的方法
+  const getCategory = async () => {
+    const res = await getCategoryAPI();
+    categoryList.value = res.result;
+  };
+
+  return {
+    categoryList,
+    getCategory,
+  };
+});
