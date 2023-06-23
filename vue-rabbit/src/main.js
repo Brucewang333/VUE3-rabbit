dc3d5a681e9f0abb862ae18a390dfb0bc65ec814
Bruce <wang922711@163.com>
2023-06-23 17:01:14 +0800

    图片懒加载指令实现

--- vue-rabbit/src/main.js
+++ vue-rabbit/src/main.js
@@ -1,21 +1,35 @@
 // 引入初始化样式文件
-import '@/styles/common.scss'
+import "@/styles/common.scss";
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
 
-import App from './App.vue'
-import router from './router'
+import App from "./App.vue";
+import router from "./router";
+import { useIntersectionObserver } from "@vueuse/core";
 
 // 测试接口函数
 // import { get } from './apis/testAPI'
 // get().then(res => {
-    // console.log(res)
+// console.log(res)
 // })
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(createPinia())
-app.use(router)
+app.use(createPinia());
+app.use(router);
 
-app.mount('#app')
+app.mount("#app");
+
+// 定义全局指令
+
+app.directive("img-lazy", {
+  mounted(el, binding) {
+    useIntersectionObserver(el, ([{ isIntersecting }]) => {
+      if (isIntersecting) {
+        // 进入视口区域
+        el.src = binding.value;
+      }
+    });
+  },
+});
