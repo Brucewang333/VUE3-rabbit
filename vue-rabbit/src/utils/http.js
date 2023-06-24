05287d289e5bdf9f0084121a9c21aa29d285c456
Bruce <wang922711@163.com>
2023-06-25 00:31:21 +0800

    表单验证及登录功能

--- vue-rabbit/src/utils/http.js
+++ vue-rabbit/src/utils/http.js
@@ -1,7 +1,8 @@
 // axios基础的封装
 
 import axios from "axios";
-
+import { ElMessage } from 'element-plus'
+import 'element-plus/theme-chalk/el-message.css'
 const httpInstance = axios.create({
   baseURL: "http://pcapi-xiaotuxian-front-devtest.itheima.net",
   timeout: 5000,
@@ -26,6 +27,11 @@ httpInstance.interceptors.request.use(
 httpInstance.interceptors.response.use(
   (res) => res.data,
   (e) => {
+    // 统一错误提示
+    ElMessage({
+        type: 'warning',
+        message: e.response.data.message
+    })
     return Promise.reject(e);
   }
 );
