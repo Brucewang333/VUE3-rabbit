ddc56b129602fa2d9be6e09ca929975b43147521
Bruce <wang922711@163.com>
2023-06-24 12:48:12 +0800

    路由滚动优化

--- vue-rabbit/src/router/index.js
+++ vue-rabbit/src/router/index.js
@@ -9,6 +9,7 @@ import SubCategory from '@/views/SubCategory/index.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+  // path和component对应关系的位置
   routes: [
     {
         path:'/',
@@ -34,7 +35,12 @@ const router = createRouter({
         component:Login
     }
     
-  ]
+  ],
+  // 路由滚动行为
+   scrollBehavior() {
+    return { top: 0 }
+  
+   }
 })
 
 export default router
